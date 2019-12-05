fcc6b10a30a211529ba36b9c26fdca6a3f95873e
Tristan Matthias <hello@tristanmatthias.com>
2019-12-05 16:03:43 -0500

    fix(cli): port config issue

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -1,6 +1,7 @@
 import { CommandBuilder, CommandModule } from 'yargs';
 
 import { start } from '../lib/start';
+import { API_CONFIG } from '@blokz/api';
 
 
 export const command = ['start [dir]', '$0 [dir]'];
@@ -32,9 +33,10 @@ interface StartArgs {
 export const handler: CommandModule<any, StartArgs>['handler'] = async args => {
   const { dir, skipDB, port } = args;
 
-  start(dir, {
-    skipDatabase: skipDB,
-    port
-  });
+  const config: Partial<API_CONFIG> = {
+    skipDatabase: skipDB
+  };
+  if (port) config.port = port;
+  start(dir, config);
 };
 
