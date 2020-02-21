660e55284eaf1e06d03e3bf4ed5d64892de72f04
Tristan Matthias <hello@tristanmatthias.com>
2020-02-21 16:48:42 -0500

    feat(plugin-entity-user): added user entity plugin

---
+++ packages/plugin-entity-user/src/entities/User.entity.ts
@@ -0,0 +1,64 @@
+import { Model, ModelField } from '@brix/model';
+import { Store } from '@brix/model/dist/Store';
+import { Arg, Field, InputType, Mutation, ObjectType, Query, Resolver, Authorized } from 'type-graphql';
+import { hashPassword } from '@brix/core';
+
+
+@ObjectType()
+@Model()
+export class User {
+  @Field()
+  id: string;
+
+  @Field()
+  @ModelField()
+  firstName: string;
+
+  @Field()
+  @ModelField()
+  lastName: string;
+
+  @Field()
+  @ModelField()
+  email: string;
+
+  @ModelField()
+  password: string;
+}
+
+
+@InputType()
+export class UserInput {
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
+
+
+@Resolver(User)
+export class UserResolver {
+  model = Store.model<User>('User');
+
+  @Query(() => [User])
+  @Authorized()
+  async users() {
+    return this.model.findAll();
+  }
+
+  // TODO: Make more secure and only run once
+  @Mutation(() => User)
+  async signup(
+    @Arg('user', () => UserInput) user: UserInput
+  ) {
+    user.password = await hashPassword(user.password);
+    return this.model.create(user);
+  }
+}
