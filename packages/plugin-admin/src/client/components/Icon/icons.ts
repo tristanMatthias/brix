4aee5c610ec3eb14a95c2eccd915d8dddbbe1fb7
Tristan Matthias <hello@tristanmatthias.com>
2020-03-01 17:12:42 -0500

    feat(plugin-admin): added page icon

--- packages/plugin-admin/src/client/components/Icon/icons.ts
+++ packages/plugin-admin/src/client/components/Icon/icons.ts
@@ -1,17 +1,18 @@
+import uploadDuotone from '../../images/icons/duotone/cid-cloud-upload.svg';
 import images from '../../images/icons/duotone/cid-images.svg';
 import error from '../../images/icons/duotone/cid-warning.svg';
 import logout from '../../images/icons/linear/cil-account-logout.svg';
 import minus from '../../images/icons/linear/cil-minus.svg';
+import page from '../../images/icons/linear/cil-page.svg';
 import userLine from '../../images/icons/linear/cil-user.svg';
 import check from '../../images/icons/solid/cis-check.svg';
 import chevronBottomAlt from '../../images/icons/solid/cis-chevron-bottom-alt.svg';
 import chevronTopAlt from '../../images/icons/solid/cis-chevron-top-alt.svg';
+import upload from '../../images/icons/solid/cis-cloud-upload.svg';
 import exclamation from '../../images/icons/solid/cis-exclamation.svg';
 import lock from '../../images/icons/solid/cis-lock-minus.svg';
 import plus from '../../images/icons/solid/cis-plus.svg';
 import user from '../../images/icons/solid/cis-user.svg';
-import uploadDuotone from '../../images/icons/duotone/cid-cloud-upload.svg';
-import upload from '../../images/icons/solid/cis-cloud-upload.svg';
 
 
 export const icons = {
@@ -29,5 +30,6 @@ export const icons = {
   userLine,
   upload,
   uploadDuotone,
-  plus
+  plus,
+  page
 };
