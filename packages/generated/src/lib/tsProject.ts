57746bf972fb496f99d22091dc22845696e5f8de
Tristan Matthias <hello@tristanmatthias.com>
2020-03-03 14:56:13 -0500

    fix(generated): fix generated ts location

--- packages/generated/src/lib/tsProject.ts
+++ packages/generated/src/lib/tsProject.ts
@@ -1,46 +1,50 @@
-import path from 'path';
 import fs from 'fs-extra';
-import { Project, ScriptTarget, ModuleKind, ModuleResolutionKind } from 'ts-morph';
-import { } from 'typescript';
+import { ModuleKind, ModuleResolutionKind, Project, ScriptTarget } from 'ts-morph';
 
-export const tsProject = new Project({
-  addFilesFromTsConfig: false,
-  compilerOptions: {
-    outDir: path.resolve(__dirname, '../../'),
-    rootDir: path.resolve(__dirname, '../../src'),
-    declaration: true,
-    target: ScriptTarget.ESNext,
+import { getDir } from './getDir';
+
+let tsProject: Project;
+
+const setup = async () => {
+  tsProject = new Project({
+    addFilesFromTsConfig: false,
+    compilerOptions: {
+      outDir: await getDir(),
+      rootDir: await getDir('src'),
+      declaration: true,
+      target: ScriptTarget.ESNext,
 
 
-    module: ModuleKind.CommonJS,
-    strict: true,
-    noUnusedLocals: true,
-    esModuleInterop: true,
-    experimentalDecorators: true,
-    emitDecoratorMetadata: true,
-    resolveJsonModule: true,
-    forceConsistentCasingInFileNames: true,
-    composite: false,
-    sourceMap: true,
-    moduleResolution: ModuleResolutionKind.NodeJs,
-    removeComments: false,
-    noImplicitAny: true,
-    strictNullChecks: true,
-    strictFunctionTypes: true,
-    strictPropertyInitialization: false,
-    noImplicitThis: true,
-    noUnusedParameters: true,
-    noImplicitReturns: true,
-    noFallthroughCasesInSwitch: true
-  }
-});
-
+      module: ModuleKind.CommonJS,
+      strict: true,
+      noUnusedLocals: true,
+      esModuleInterop: true,
+      experimentalDecorators: true,
+      emitDecoratorMetadata: true,
+      resolveJsonModule: true,
+      forceConsistentCasingInFileNames: true,
+      composite: false,
+      sourceMap: true,
+      moduleResolution: ModuleResolutionKind.NodeJs,
+      removeComments: false,
+      noImplicitAny: true,
+      strictNullChecks: true,
+      strictFunctionTypes: true,
+      strictPropertyInitialization: false,
+      noImplicitThis: true,
+      noUnusedParameters: true,
+      noImplicitReturns: true,
+      noFallthroughCasesInSwitch: true
+    }
+  });
+};
 
 export const compileFile = async (filename: string, source: string) =>
   compile(filename, (await fs.readFile(source)).toString());
 
 
 export const compile = async (filename: string, contents: string) => {
+  if (!tsProject) await setup();
   const file = tsProject.createSourceFile(filename, contents, { overwrite: true });
   const result = await file.emit();
 
