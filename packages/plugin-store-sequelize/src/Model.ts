582b7146ea6152f4897665fc63784d6896a9d443
Tristan Matthias <hello@tristanmatthias.com>
2020-03-08 17:23:52 -0400

    fix(plugin-store-sequelize): fixed json field

--- packages/plugin-store-sequelize/src/Model.ts
+++ packages/plugin-store-sequelize/src/Model.ts
@@ -2,33 +2,36 @@ import { BrixStoreModel, BrixStoreModelFindOptions } from '@brix/model';
 import Seq from 'sequelize';
 import { FindOptions } from 'sequelize/types';
 
+const globalOptions: FindOptions = { plain: true, raw: true };
 export class SequelizeModel<T> implements BrixStoreModel<T> {
 
   constructor(
     private _seqModel: Seq.ModelCtor<Seq.Model<T, any>>) { }
 
   async findAll() {
-    return (await this._seqModel.findAll()) as unknown as T[];
+    return (await this._seqModel.findAll()).map(i => i.toJSON()) as unknown as T[];
   }
 
   async create(data: object) {
-    return (await this._seqModel.create(data)) as unknown as T;
+    return (await this._seqModel.create(data, globalOptions)).toJSON() as unknown as T;
   }
 
   async findById(id: string) {
-    return (await this._seqModel.findByPk(id)) as unknown as T;
+    return (await this._seqModel.findByPk(id))?.toJSON() as unknown as T;
   }
 
   async findOne(options: BrixStoreModelFindOptions<T>) {
-    return (await this._seqModel.findOne(options as FindOptions)) as unknown as T;
+    return (await this._seqModel.findOne({
+      ...options as FindOptions
+    }))?.toJSON() as unknown as T;
   }
 
   async deleteById(id: string) {
-    return Boolean(await this._seqModel.destroy({ where: { id } }));
+    return Boolean(await this._seqModel.destroy({ where: { id }, ...globalOptions }));
   }
 
   async updateById(id: string, values: Partial<T>) {
-    const updated = await this._seqModel.update({ values }, { where: { id } });
-    return updated[0] as unknown as T;
+    await this._seqModel.update({ ...values }, { where: { id }, ...globalOptions });
+    return this.findById(id);
   }
 }
